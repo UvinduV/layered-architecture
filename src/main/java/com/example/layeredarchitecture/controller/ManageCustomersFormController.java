b4ecf044a91c2849504371eef5befdb0a2dd1b60
UvinduV <uvindusanketh470@gmail.com>
2023-12-17 12:20:56 +0530

    refactor save customer

--- src/main/java/com/example/layeredarchitecture/controller/ManageCustomersFormController.java
+++ src/main/java/com/example/layeredarchitecture/controller/ManageCustomersFormController.java
@@ -150,12 +150,14 @@ public class ManageCustomersFormController {
                 if (existCustomer(id)) {
                     new Alert(Alert.AlertType.ERROR, id + " already exists").show();
                 }
-                Connection connection = DBConnection.getDbConnection().getConnection();
+                CustomerDAOImpl customerDAO=new CustomerDAOImpl();
+                boolean isSaved= customerDAO.saveCustomer(id,name,address);
+                /*Connection connection = DBConnection.getDbConnection().getConnection();
                 PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer (id,name, address) VALUES (?,?,?)");
                 pstm.setString(1, id);
                 pstm.setString(2, name);
                 pstm.setString(3, address);
-                pstm.executeUpdate();
+                pstm.executeUpdate();*/
 
                 tblCustomers.getItems().add(new CustomerTM(id, name, address));
             } catch (SQLException e) {
