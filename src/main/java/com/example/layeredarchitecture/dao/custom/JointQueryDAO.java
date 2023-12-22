cbc9495449706f49deb501bd93f55f6548fdc47d
UvinduV <uvindusanketh470@gmail.com>
2023-12-22 09:09:23 +0530

    add joint Query DAO

---
+++ src/main/java/com/example/layeredarchitecture/dao/custom/JointQueryDAO.java
@@ -0,0 +1,16 @@
+package com.example.layeredarchitecture.dao.custom;
+
+import com.example.layeredarchitecture.dao.SqlUtil;
+import com.example.layeredarchitecture.model.CustOrderQueryDTO;
+import com.example.layeredarchitecture.model.OrderItemDTO;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface JointQueryDAO {
+    ArrayList<CustOrderQueryDTO> search(String id) throws SQLException, ClassNotFoundException ;
+
+    ArrayList<OrderItemDTO> searchOrderItem(String oid) throws SQLException, ClassNotFoundException ;
+
+}
--- src/main/java/com/example/layeredarchitecture/dao/impl/JointQueryDAOImpl.java
+++ src/main/java/com/example/layeredarchitecture/dao/impl/JointQueryDAOImpl.java
@@ -1,6 +1,7 @@
 package com.example.layeredarchitecture.dao.impl;
 
 import com.example.layeredarchitecture.dao.SqlUtil;
+import com.example.layeredarchitecture.dao.custom.JointQueryDAO;
 import com.example.layeredarchitecture.model.CustOrderQueryDTO;
 import com.example.layeredarchitecture.model.OrderItemDTO;
 
@@ -9,7 +10,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class JointQueryDAOImpl {
+public class JointQueryDAOImpl implements JointQueryDAO {
+    @Override
     public ArrayList<CustOrderQueryDTO> search(String id) throws SQLException, ClassNotFoundException {
         ResultSet rst = SqlUtil.test("select o.oid,c.name from Customer c join Orders o on c.id = o.customerID where o.customerID = ?",id);
 
@@ -24,7 +26,8 @@ public class JointQueryDAOImpl {
     }
 
 
-    public ArrayList<OrderItemDTO> searchOrderItem(String oid) throws SQLException, ClassNotFoundException {
+   @Override
+   public ArrayList<OrderItemDTO> searchOrderItem(String oid) throws SQLException, ClassNotFoundException {
         ResultSet rst=SqlUtil.test("select o.date,od.itemCode,i.description,i.qtyOnHand,i.unitPrice from Orders o\n" +
                 "    join OrderDetails od on o.oid = od.oid\n" +
                 "    join Item i on od.itemCode = i.code\n" +
